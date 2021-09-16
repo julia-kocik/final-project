bf301ad911e9191ede77edac2688e2c528eee5c5
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2021-09-16 16:15:09 +0200

    Add NavBar component and styles

--- src/components/layout/NavBar/NavBar.js
+++ src/components/layout/NavBar/NavBar.js
@@ -1,22 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {TopBar} from '../TopBar/TopBar';
-import {Logo} from '../../features/Logo/Logo';
-import {NavBar} from '../NavBar/NavBar';
+import {NavLink} from 'react-router-dom';
+
 import clsx from 'clsx';
 
 // import { connect } from 'react-redux';
 // import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
 
-import styles from './MainLayout.module.scss';
+import styles from './NavBar.module.scss';
 
 const Component = ({className, children}) => (
   <div className={clsx(className, styles.root)}>
-    <TopBar />
-    <Logo logo="https://cdn.pixabay.com/photo/2015/05/30/14/27/yorkshire-terrier-790361_1280.jpg"/>
-    <NavBar/>
-    <h2>MainLayout</h2>
-    {children}
+    <NavLink to='/home'className={styles.navLink}>Home</NavLink>
+    <NavLink to='/shop' className={styles.navLink}>Shop</NavLink>
+    <NavLink to='/about' className={styles.navLink}>About</NavLink>
   </div>
 );
 
@@ -36,7 +33,7 @@ Component.propTypes = {
 // const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
 
 export {
-  Component as MainLayout,
-  // Container as MainLayout,
-  Component as MainLayoutComponent,
+  Component as NavBar,
+  // Container as NavBar,
+  Component as NavBarComponent,
 };
