6a1529443b71ded281efe4aa90f0c6df75e4afa7
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2021-09-23 11:37:37 +0200

    Refactor backend files

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,10 @@
+require("dotenv").config();
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const mongoose = require('mongoose');
 
-const productsRoutes = require('./routes/products.routes');
-const cartsRoutes = require('./routes/carts.routes');
-
+const productsRoutes = require('./routes/productRoutes');
+const connectDB = require("./config/db");
 const app = express();
 
 /* MIDDLEWARE */
@@ -15,7 +14,6 @@ app.use(express.urlencoded({ extended: false }));
 
 /* API ENDPOINTS */
 app.use('/api', productsRoutes);
-app.use('/api', cartsRoutes);
 
 /* API ERROR PAGES */
 app.use('/api', (req, res) => {
@@ -29,12 +27,7 @@ app.use('*', (req, res) => {
 });
 
 /* MONGOOSE */
-mongoose.connect('mongodb://localhost:27017/finalProject', { useNewUrlParser: true, useUnifiedTopology: true });
-const db = mongoose.connection;
-db.once('open', () => {
-  console.log('Successfully connected to the database');
-});
-db.on('error', err => console.log('Error: ' + err));
+connectDB();
 
 /* START SERVER */
 const port = process.env.PORT || 8000;
